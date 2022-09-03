95c33b702ed6d75056967d5664c759aba02fb274
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-09-03 10:01:38 +0300

    First steps in coding

---
+++ Programming Basics with C# - юли 2022/01First Steps In Coding - Lab/04. Inches to Centimeters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Inches_to_Centimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read input from the user
+
+            double numbers = double.Parse(Console.ReadLine());
+
+            // Logical conversion
+
+            double inches = 2.54;
+            double centimeters = numbers * inches;
+
+            // Printing to result on the console
+
+            Console.WriteLine(centimeters);
+        }
+    }
+}
