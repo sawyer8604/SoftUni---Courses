ac956625338de96e829320f051e5b7f6a431aa6f
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-09-07 19:55:48 +0300

    Tasks from Arrays - Lab

---
+++ Programming Fundamentals - септември 2022/Arrays/01. Day of Week/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDay = int.Parse(Console.ReadLine());
+
+            string [] days =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            if (numberOfDay > 0 && numberOfDay <= 7)
+            {
+
+                Console.WriteLine(days[numberOfDay -1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - септември 2022/Arrays/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int[] number = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                number[i] = int.Parse(Console.ReadLine());
+                                
+            }
+            for (int i = length -1; i >= 0; i--)
+            {
+                Console.Write($"{number[i]} ");
+
+            }
+                        
+
+        }
+    }
+}
