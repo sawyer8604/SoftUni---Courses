e6b72e2224a3bab4ff3eb2a823bf7904a28a105a
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-10-02 11:42:53 +0300

    All tasks from Lists - Exercise

---
+++ Programming Fundamentals - септември 2022/Lists - Exercise/06. Cards Game/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Cards_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstPlayerCard = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondPlayerCard = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
+            while (firstPlayerCard.Count > 0 && secondPlayerCard.Count > 0)
+            {
+                                
+                if (firstPlayerCard[0] > secondPlayerCard[0])
+                {
+                    firstPlayerCard.Add(firstPlayerCard[0]);
+                    firstPlayerCard.Add(secondPlayerCard[0]);                    
+                        
+                }
+                else if(firstPlayerCard[0] < secondPlayerCard[0])
+                {
+                    secondPlayerCard.Add(secondPlayerCard[0]);
+                    secondPlayerCard.Add(firstPlayerCard[0]);
+                    
+                }
+                
+                    firstPlayerCard.RemoveAt(0);
+                    secondPlayerCard.RemoveAt(0);               
+
+            }
+            
+
+            if(firstPlayerCard.Count > secondPlayerCard.Count)
+            {
+                Console.WriteLine($"First player wins! Sum: {firstPlayerCard.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Second player wins! Sum: {secondPlayerCard.Sum()}");
+
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - септември 2022/Lists - Exercise/07. Append Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numberAsString = Console.ReadLine().Split("|").Reverse().ToList();
+
+            List<int> numbers = new List<int>();            
+
+            foreach (string number in numberAsString)
+            {
+                numbers.AddRange(number.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            }
+            
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+    }
+}
