62e47e57914b663296c704407a6a0a555c196bea
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-10-03 09:57:28 +0300

    All tasks from List - More Exercise

---
+++ Programming Fundamentals - септември 2022/Lists - More Exercise/04. Mixed up Lists/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Mixed_up_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> mixedList = new List<int>();
+
+            int minCount = Math.Min(firstList.Count, secondList.Count);
+            int maxCount = Math.Max(firstList.Count, secondList.Count);
+
+            int count = 0;
+
+            while (minCount > 0)
+            {
+                minCount--;
+                maxCount--;
+
+                if (firstList.Count > secondList.Count)
+                {
+                    mixedList.Add(firstList[count]);
+                    mixedList.Add(secondList[minCount]);
+
+                    firstList.RemoveAt(count);
+                    secondList.RemoveAt(minCount);
+                }
+                else
+                {
+                    mixedList.Add(firstList[count]);
+                    mixedList.Add(secondList[maxCount]);
+
+                    firstList.RemoveAt(count);
+                    secondList.RemoveAt(maxCount);
+
+                }
+            }
+
+            if(firstList.Count > 0)
+            {
+                int firstElement = firstList[0];
+                int secondElement = firstList[1];
+                int maxElement = Math.Max(secondElement, firstElement);
+                int minElement = Math.Min(secondElement, firstElement);
+
+                mixedList.Sort();
+
+                for (int i = 0 + 1; i < mixedList.Count; i++)
+                {
+                    if (mixedList[i] > minElement && mixedList[i] < maxElement)
+                    {
+                        Console.Write($"{mixedList[i]} ");
+                    }
+                    
+                }
+            }
+            else
+            {
+                int firstElement = secondList[0];
+                int secondElement = secondList[1];
+                int maxElement = Math.Max(secondElement, firstElement);
+                int minElement = Math.Min(secondElement, firstElement);
+
+                mixedList.Sort();
+
+                for (int i = 0 + 1; i < mixedList.Count; i++)
+                {
+                    if (mixedList[i] > minElement && mixedList[i] < maxElement)
+                    {
+                        Console.Write($"{mixedList[i]} ");
+                    }
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - септември 2022/Lists - More Exercise/05. Drum Set/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Drum_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double savingMoney = double.Parse(Console.ReadLine());
+
+            List<int> quality = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> originalQuality = new List<int>();
+
+            string command = Console.ReadLine();
+
+            for (int i = 0; i < quality.Count; i++)
+            {
+                originalQuality.Add(quality[i]);
+            }
+
+            while (command != "Hit it again, Gabsy!")
+            {
+
+                int input = int.Parse(command);
+
+
+                for (int i = 0; i < quality.Count; i++)
+                {
+
+                    quality[i] -= input;
+
+                    if (quality[i] <= 0)
+                    {
+                        if (savingMoney - originalQuality[i] * 3 >= 0)
+                        {
+                            savingMoney = savingMoney - originalQuality[i] * 3;
+                            quality[i] = originalQuality[i];
+                        }
+
+                    }
+
+                }
+                for (int i = 0; i < originalQuality.Count; i++)
+                {
+                    if (quality[i] <= 0)
+                    {
+                        quality.Remove(quality[i]);
+                        originalQuality.Remove(originalQuality[i]);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+            
+
+            Console.WriteLine(String.Join(" ", quality));
+            Console.WriteLine($"Gabsy has {savingMoney:f2}lv.");
+        }
+    }
+}
