f9704aac908183d6774aaffdd29b6eeeebb2d301
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-10-11 06:52:59 +0300

    Add tasks from Arrays - More Exercise

--- Programming Fundamentals - септември 2022/Arrays - More Exercise/03. Safe Manipulation/Program.cs
+++ Programming Fundamentals - септември 2022/Arrays - More Exercise/03. Safe Manipulation/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Linq;
 
-namespace _03._Safe_Manipulation
+namespace _02._Manipulate_Array
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] commands = command.Split();
+
+                string firstCommand = commands[0];
+
+                switch (firstCommand)
+                {
+                    case "Reverse":
+
+                        Array.Reverse(input);
+
+                        break;
+
+                    case "Distinct":
+
+                        string[] distinctArr = input.Distinct().ToArray();
+                        input = distinctArr;
+                        break;
+                    case "Replace":
+
+                        int index = int.Parse(commands[1]);
+                        string element = commands[2];
+
+                        if (index >= 0 && index < input.Length)
+                        {
+                            input[index] = element;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input!");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input!");
+                        break;
+
+                }
+            }
+            Console.WriteLine(String.Join(", ", input));
         }
     }
 }
---
+++ Programming Fundamentals - септември 2022/Arrays - More Exercise/05. Pizza Ingredients/Program.cs
@@ -0,0 +1,58 @@
+﻿
+namespace _05._Pizza_Ingredients
+{
+    using System;
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ingredients = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int searchedLenth = int.Parse(Console.ReadLine()); 
+
+            while (true)
+            {
+                string allElements = string.Empty;
+                int counter = 0;
+
+                for (int i = 0; i < ingredients.Length; i++)
+                {
+                    string currentIngredient = ingredients[i];
+
+
+                    if (currentIngredient.Length == searchedLenth)
+                    {
+                        counter++;
+                        allElements += currentIngredient + ", ";
+                        Console.WriteLine($"Adding {currentIngredient}.");
+
+                    }
+
+                }
+                                
+
+                int index = allElements.LastIndexOf(',');
+                string newelements = allElements.Remove(index, 1);
+
+                int space = newelements.LastIndexOf(' ');
+                string newelements2 = newelements.Remove(space, 1);
+                
+
+
+                Console.WriteLine($"Made pizza with total of {counter} ingredients.");
+                Console.WriteLine($"The ingredients are: {newelements2}.");
+
+                if (counter >= 10)
+                {
+                    break;
+                }
+
+
+                ingredients = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                searchedLenth = int.Parse(Console.ReadLine());
+
+            }
+        }
+    }
+}
