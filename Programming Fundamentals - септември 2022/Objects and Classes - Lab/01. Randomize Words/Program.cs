85b85f300a8fed8c95787dcce2616f075c920544
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-10-25 16:18:47 +0300

    Add tasks from Objects and Classes - Lab

---
+++ Programming Fundamentals - септември 2022/Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Random random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i];
+
+                int randomIndex = random.Next(words.Length);
+
+                words[i] = words[randomIndex];
+
+                words[randomIndex] = currentWord;
+
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
---
+++ Programming Fundamentals - септември 2022/Objects and Classes - Lab/03. Songs/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Song> songs = new List<Song>();
+
+            int numberOfSong = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 0; i < numberOfSong; i++)
+            {
+                string[] commands = Console.ReadLine().Split('_');
+
+                string type = commands[0];
+                string name = commands[1];
+                string time = commands[2];
+
+                Song song = new Song();
+
+                song.TypeList = type;
+                song.Name = name;
+                song.Time = time;
+
+                songs.Add(song);
+
+            }
+
+            string typeList = Console.ReadLine();
+
+            if(typeList == "all")
+            {
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+
+            }
+            else
+            {
+                List<Song> filteredTypeList = songs.Where(song => song.TypeList == typeList).ToList();
+
+                foreach (Song song in filteredTypeList)
+                {
+                    Console.WriteLine(song.Name);
+
+                }
+
+            }
+        }
+    }
+
+    class Song
+    {
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+
+
+    }
+}
