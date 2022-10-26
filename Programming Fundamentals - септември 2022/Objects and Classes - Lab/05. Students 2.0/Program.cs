88debe04a618aa333bb8d7c1d348563d821aebab
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-10-26 10:20:41 +0300

    Add solutions

---
+++ Programming Fundamentals - септември 2022/Objects and Classes - Lab/05. Students 2.0/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Students_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] cmdArguments = command.Split(' ');
+
+
+                if (IsStudentExist(cmdArguments[0], cmdArguments[1], students))
+                {
+                    Student student = students.Find(student => student.FirstName == cmdArguments[0] && student.LastName == cmdArguments[1]);
+                    student.Age = int.Parse(cmdArguments[2]);
+                    student.HomeTown = cmdArguments[3];
+                }
+                else
+                {
+
+                    Student student = new Student(cmdArguments[0], cmdArguments[1], int.Parse(cmdArguments[2]), cmdArguments[3]);
+                    students.Add(student);
+                }
+            }
+
+            string filter = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (student.HomeTown == filter)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+
+
+        static bool IsStudentExist(string firstName, string lastName, List<Student> students)
+        {
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+
+        }
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, int age, string homeTown)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.HomeTown = homeTown;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+
+
+    }
+}
---
+++ Programming Fundamentals - септември 2022/Objects and Classes - Lab/06. Store Boxes/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Store_Boxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> boxes = new List<Box>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] cmdArguments = command.Split(' ');
+
+                Box box = new Box
+                {
+                    SerialNumber = cmdArguments[0],
+                    Item = new Item(cmdArguments[1], decimal.Parse(cmdArguments[3])),
+                    ItemQuantity = int.Parse(cmdArguments[2])
+                };
+
+                boxes.Add(box);
+
+            }
+
+            List<Box> orderedBoxes = boxes.OrderByDescending(box => box.Price).ToList();
+
+            foreach (Box box in orderedBoxes)
+            {
+                Console.WriteLine(box.SerialNumber);
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:f2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.Price:f2}");
+            }
+        }
+    }
+
+    class Item
+    {
+        public Item(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+
+    class Box
+    {
+        public string SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal Price
+        { 
+            get 
+            {
+                return this.ItemQuantity * this.Item.Price;
+            }
+        }
+
+    }
+}
