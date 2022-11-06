16716a2c0f894fcc4d24ff7b62490fe84be3419b
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-11-06 15:45:59 +0200

    Add solutions

---
+++ Programming Fundamentals - септември 2022/Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.Write($"{match} ");
+
+            }
+        }
+    }
+}
