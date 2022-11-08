8e5b0e5a0c6db05878b5fb1f34914f0edc086c86
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-11-08 20:52:45 +0200

    Add solutions

---
+++ Programming Fundamentals - септември 2022/Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>(?<name>[A-Za-z]+)<<(?<price>[\d].*[\d])\!(?<quantity>[0-9]+)";
+
+            string input;
+
+            double totalMoney = 0;
+
+            List<string> furnitures = new List<string>();   
+
+            while ((input = Console.ReadLine()) != "Purchase")
+            {
+                Match match = Regex.Match(input, pattern);
+
+                if(match.Success)
+                {
+                    string name = match.Groups["name"].Value;
+                    double price = double.Parse(match.Groups["price"].Value);
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+
+
+                    furnitures.Add(name);
+
+                    totalMoney += price * quantity;
+
+                }
+
+            }
+
+            Console.WriteLine("Bought furniture:");
+
+            foreach (string furniture in furnitures)
+            {
+                Console.WriteLine(furniture);
+            }
+
+            Console.WriteLine($"Total money spend: {totalMoney:f2}");
+        }
+    }
+}
---
+++ Programming Fundamentals - септември 2022/Regular Expressions - Exercise/02. Race/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace _02._Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(", ").ToList();
+
+
+            Dictionary<string, int> race = new Dictionary<string, int>();
+
+            int distance = 0;
+            string input;
+
+
+            while ((input = Console.ReadLine()) != "end of race")
+            {
+                char[] letters = input.ToCharArray();
+
+                StringBuilder sb = new StringBuilder();
+
+                string crrName;
+                distance = 0;
+
+                foreach (var letter in letters)
+                {
+                    if (char.IsLetter(letter))
+                    {
+                        sb.Append(letter);
+
+                    }
+
+                    if(char.IsDigit(letter))
+                    {
+                        distance += letter - 48;
+                    }
+                }
+                crrName = sb.ToString();
+
+                if(names.Contains(crrName))
+                {
+                    if(!race.ContainsKey(crrName))
+                    {
+                        race[crrName] = distance;
+                    }
+                    else
+                    {
+                        race[crrName] += distance;
+                    }
+                }
+               
+            }
+            race.OrderByDescending(r => r.Value).Take(3);
+
+
+            var result = race.OrderByDescending(x => x.Value).Take(3);
+
+            int count = 1;
+
+            foreach (var item in result)
+            {
+                string place = count == 1 ? "st" : count == 2 ? "nd" : "rd";
+                Console.WriteLine($"{count}{place} place: {item.Key}");
+                count++;
+            }
+
+        }
+    }
+}
