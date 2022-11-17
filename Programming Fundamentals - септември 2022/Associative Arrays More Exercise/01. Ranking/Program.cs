e7a0041b2883835cfad8d3558d4c61c013dca347
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-11-17 13:39:28 +0200

    Add solutions

---
+++ Programming Fundamentals - септември 2022/Associative Arrays More Exercise/01. Ranking/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _01._Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            char[] chars = { ':', '=', '>' };
+
+            Dictionary<string, string> contests = new Dictionary<string, string>();
+
+            while ((input = Console.ReadLine()) != "end of contests")
+            {
+                string[] inputArguments = input.Split(":");
+
+                string contest = inputArguments[0];
+                string password = inputArguments[1];
+
+                if (!contests.ContainsKey(contest))
+                {
+                    contests[contest] = password;
+                }
+
+
+            }
+            Dictionary<string, Dictionary<string, double>> users = new Dictionary<string, Dictionary<string, double>>();
+
+            string commands;
+
+            while ((commands = Console.ReadLine()) != "end of submissions")
+            {
+                string[] cmdArguments = commands.Split("=>");
+
+                string contest = cmdArguments[0];
+                string password = cmdArguments[1];
+                string userName = cmdArguments[2];
+                double points = double.Parse(cmdArguments[3]);
+
+                if (contests.ContainsKey(contest) && contests[contest] == password)
+                {
+                    if (!users.ContainsKey(userName))
+                    {
+                        users[userName] = new Dictionary<string, double>();
+                    }
+
+                    if (!users[userName].ContainsKey(contest))
+                    {
+                        users[userName][contest] = 0;
+                    }
+
+                    if (users[userName][contest] < points)
+                    {
+                        users[userName][contest] = points;
+
+                    }
+
+                }
+            }
+
+            string winner = users.OrderBy(x => x.Value.Values.Sum()).Last().Key;
+            double bestPoints = users.OrderBy(x => x.Value.Values.Sum()).Last().Value.Values.Sum();
+
+            Console.WriteLine($"Best candidate is {winner} with total {bestPoints} points.");
+
+            Console.WriteLine("Ranking: ");
+
+            foreach (var user in users.OrderBy(w => w.Key))
+            {
+                Console.WriteLine(user.Key);
+
+                foreach (var (contest, point) in user.Value.OrderByDescending(point => point.Value))
+                {
+                    Console.WriteLine($"#  {contest} -> {point}");
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - септември 2022/Associative Arrays More Exercise/02. Judge/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> users = new Dictionary<string, Dictionary<string, int>>();
+
+            SortedDictionary<string, Dictionary<string, int>> individualStatistics = new SortedDictionary<string, Dictionary<string, int>>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "no more time")
+            {
+                string[] inputArguments = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                string userName = inputArguments[0];
+                string contest = inputArguments[1];
+                int points = int.Parse(inputArguments[2]);
+
+                if (!users.ContainsKey(contest))
+                {
+                    users[contest] = new Dictionary<string, int>();
+                }
+
+                if (!users[contest].ContainsKey(userName))
+                {
+                    users[contest][userName] = points;
+
+                }
+                else
+                {
+                    if (users[contest][userName] < points)
+                    {
+                        users[contest][userName] = points;
+                    }
+                }
+
+                if (!individualStatistics.ContainsKey(userName))
+                {
+                    individualStatistics.Add(userName, new Dictionary<string, int>());
+                    individualStatistics[userName].Add(contest, points);
+                }
+                else
+                {
+
+
+                    if (!individualStatistics[userName].ContainsKey(contest))
+                    {
+                        individualStatistics[userName][contest] = points;
+                    }
+                    else
+                    {
+
+                        if (individualStatistics[userName][contest] < points)
+                        {
+                            individualStatistics[userName][contest] = points;
+                        }
+                    }
+                }
+
+
+
+            }
+
+
+
+
+            foreach (var user in users)
+            {
+                int position1 = 0;
+
+                Console.WriteLine($"{user.Key}: {user.Value.Count} participants");
+
+                foreach (var (practicipant, points) in user.Value.OrderByDescending(point => point.Value).ThenBy(name => name.Key))
+                {
+                    position1++;
+                    Console.WriteLine($"{position1}. {practicipant} <::> {points}");
+                }
+            }
+
+
+
+
+            var filterIndivDic = new Dictionary<string, int>();
+
+            int sum = 0;
+
+            foreach (var item in individualStatistics)
+            {
+                foreach (var items in item.Value)
+                {
+                    sum += items.Value;
+                }
+                filterIndivDic.Add(item.Key, sum);
+                sum = 0;
+            }
+
+            filterIndivDic = filterIndivDic.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToDictionary(a => a.Key, b => b.Value);
+
+            int position = 1;
+            Console.WriteLine("Individual standings:");
+            foreach (var item in filterIndivDic)
+            {
+                Console.WriteLine($"{position}. {item.Key} -> {item.Value}");
+                position++;
+            }
+        }
+    }
+}
