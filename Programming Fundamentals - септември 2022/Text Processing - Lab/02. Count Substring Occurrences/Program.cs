7729ab9bb2d0b8c372bfd4433a07e3ec0e76c076
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-11-21 10:37:38 +0200

    Add solutions

---
+++ Programming Fundamentals - септември 2022/Text Processing - Lab/02. Count Substring Occurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace _02._Count_Substring_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+
+            string searchedWord = Console.ReadLine().ToLower();
+
+            int counter = 0;
+            int startIndex = 0;
+
+            while (true)
+            {
+                int index = text.IndexOf(searchedWord, startIndex);
+
+                if (index >= 0)
+                {
+                    counter++;
+                    startIndex = index +1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+                
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ Programming Fundamentals - септември 2022/Text Processing - Lab/04. Palindromes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Palindromes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimiters = new char[] { ' ', ',', '.', '?', '!' };
+
+            string[] words = Console.ReadLine().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> polindroms = new List<string>();
+
+            bool isPolindrome = false;
+
+            foreach (var word in words)
+            {
+
+                for (int i = 0; i < word.Length / 2; i++)
+                {
+                    if (word[i] == word[word.Length - 1 - i])
+                    {
+                        isPolindrome = true;
+                    }
+                    else
+                    {
+                        isPolindrome = false;
+                        break;
+                    }
+                }
+
+                
+                if (isPolindrome)
+                {
+                    polindroms.Add(word);
+                }
+
+                if (word.Length == 1)
+                {
+                    polindroms.Add(word);
+                }
+            }
+
+            polindroms = polindroms.Distinct().OrderBy(a => a).ToList();
+
+            Console.WriteLine(String.Join(", ", polindroms));
+        }
+        
+    }
+}
