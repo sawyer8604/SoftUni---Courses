aa26f9ee6ac5cb1478d4482d1ad72048b8570228
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-11-23 17:54:09 +0200

    Add solutions

---
+++ Programming Fundamentals - септември 2022/Regular Expressions - Exercise 02/01. Furniture/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @">>(?<product>[A-Za-z]+)<<(?<price>\d+(\.\d+)?)!(?<quantity>[0-9]+)";
+
+            string input;
+            Regex regex = new Regex(patern);
+
+            List<string> products = new List<string>();
+
+            double totalMoney = 0;
+
+            while ((input = Console.ReadLine()) != "Purchase")
+            {
+                Match match = regex.Match(input);
+
+                if(match.Success)
+                {
+                    string product = match.Groups["product"].Value;
+                    double price = double.Parse(match.Groups["price"].Value);
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+
+                    totalMoney += price * quantity;
+
+                    products.Add(product);
+                }
+            }
+            Console.WriteLine("Bought furniture:");
+
+            foreach (string product in products)
+            {
+                Console.WriteLine(product);
+            }
+            Console.WriteLine($"Total money spend: {totalMoney:f2}");
+        }
+    }
+}
---
+++ Programming Fundamentals - септември 2022/Regular Expressions - Exercise 02/02. Race/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace _02._Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> practicipans = Console.ReadLine().Split(", ").ToList();
+
+            string input;
+
+            Dictionary<string, int> people = new Dictionary<string, int>();
+
+
+            while ((input = Console.ReadLine()) != "end of race")
+            {
+                string name = string.Empty;
+
+                int sum = 0;
+
+                foreach (char ch in input)
+                {
+                    if (char.IsDigit(ch))
+                    {
+
+                        sum += ch - 48;
+                    }
+                }
+                StringBuilder sb = new StringBuilder();
+
+                foreach (char ch in input)
+                {
+                    if (char.IsLetter(ch))
+                    {
+
+                        sb.Append(ch);
+                    }
+                }
+
+                name = sb.ToString();
+
+                if (practicipans.Contains(name))
+                {
+                    if (!people.ContainsKey(name))
+                    {
+                        people[name] = 0;
+                    }
+                    people[name] += sum;
+
+                }
+
+
+            }
+
+            int counter = 0;
+
+
+            //var ordered = people.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            // ordered in new Dictionary
+
+
+            foreach (var person in people.OrderByDescending(km => km.Value))
+            {
+                counter++;
+                if (counter == 1)
+                {
+                    Console.WriteLine($"1st place: {person.Key}");
+                }
+                else if (counter == 2)
+                {
+                    Console.WriteLine($"2nd place: {person.Key}");
+                }
+                else if (counter == 3)
+                {
+                    Console.WriteLine($"3rd place: {person.Key}");
+                    break;
+                }
+
+            }
+        }
+    }
+}
