579fc39cc7d200670e4f8db5dfc5da671be10d75
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-11-28 16:55:38 +0200

    Add solutions

---
+++ Programming Fundamentals - септември 2022/04. Programming Fundamentals Final Exam/01. Password Reset/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Password_Reset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "Done")
+            {
+                string[] cmdArguments = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string firstCommand = cmdArguments[0];
+
+                switch (firstCommand)
+                {
+                    case "TakeOdd":
+
+                        string crrInput = "";
+
+                        for (int i = 1; i < input.Length; i++)
+                        {
+                            
+
+                            if(i % 2 != 0)
+                            {
+                                crrInput += input[i];
+                            }
+                            
+                            
+                        }
+
+                        input = crrInput;
+                        Console.WriteLine(input);
+                        break;
+
+
+                    case "Cut":
+
+                        int index = int.Parse(cmdArguments[1]);
+
+                        int length = int.Parse(cmdArguments[2]);
+
+                        input = input.Remove(index, length);
+
+                        Console.WriteLine(input);
+
+                        break;
+
+
+                    case "Substitute":
+
+                        string substring = cmdArguments[1];
+                        string substitute = cmdArguments[2];
+
+                        if(input.Contains(substring))
+                        {
+                            input = input.Replace(substring, substitute);
+                            Console.WriteLine(input);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nothing to replace!");
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine($"Your password is: {input}");
+        }
+    }
+}
---
+++ Programming Fundamentals - септември 2022/04. Programming Fundamentals Final Exam/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"\@\#+(?<word>[A-Z]{1}[A-Za-z0-9]{4,}[A-Z]{1})\@\#+";
+
+            int numberOflines = int.Parse(Console.ReadLine());
+
+            Regex regex = new Regex(patern);
+
+            for (int i = 0; i < numberOflines; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match match = regex.Match(input);
+
+                if(match.Success)
+                {
+                   string word = match.Groups["word"].Value;
+
+                    string crrLetter = "";
+
+                    foreach (char ch in word)
+                    {                        
+
+                        if(char.IsDigit(ch))
+                        {
+                            crrLetter += ch;
+                        }
+
+                    }
+
+                    if(crrLetter.Length > 0)
+                    {
+                        Console.WriteLine($"Product group: {crrLetter}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product group: 00");
+                    }                    
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+
+            }
+
+
+        }
+    }
+}
