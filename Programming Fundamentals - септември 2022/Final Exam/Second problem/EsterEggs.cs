2de8d1a9caa4b8d8a3cc1f4672e3378d8329e4f4
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-12-04 13:31:58 +0200

    initial commit

---
+++ Programming Fundamentals - септември 2022/Final Exam/Second problem/EsterEggs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace Second_problem
+{
+    internal class EsterEggs
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"([\@\#]+)(?<colour>[a-z]{3,})([\@\#]+)([^A-Za-z0-9]*)([\/]+)(?<amount>[0-9]+)([\/]+)";
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(patern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                string colur = match.Groups["colour"].Value;
+
+                string countOfEggs = match.Groups["amount"].Value;
+
+                Console.WriteLine($"You found {countOfEggs} {colur} eggs!");
+
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - септември 2022/Final Exam/Third problem/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Third_problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                string[] words = input.Split(" | ",StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in words)
+                {
+                    string[] newWord = word.Split(": ", StringSplitOptions.RemoveEmptyEntries);
+
+                    string firstWord = newWord[0];
+                    string descriptions = newWord[1];
+
+                    if(!dict.ContainsKey(firstWord))
+                    {
+                        dict[firstWord] = new List<string>();
+                    }
+
+                    dict[firstWord].Add(descriptions);
+                }
+
+                string commands = Console.ReadLine();
+                string[] cmdArguments = commands.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+
+                string lastCommand = Console.ReadLine();
+
+                if(lastCommand == "Test")
+                {
+                    foreach (var cmd in cmdArguments)
+                    {
+                        if(dict.ContainsKey(cmd))
+                        {
+                            Console.WriteLine($"{cmd}:");
+
+                            foreach (var item in dict)
+                            {
+                                if(cmd == item.Key)
+                                {
+                                    foreach (var it in item.Value)
+                                    {
+                                        Console.WriteLine($" -{it}");
+                                    }
+
+
+                                    
+                                }
+                                
+                            }
+                        }
+                    }
+                    break;
+                }
+                else if(lastCommand == "Hand Over")
+                {
+                    foreach (var item in dict)
+                    {
+                        Console.Write($"{item.Key} ");
+                    }
+                    break;
+                }
+
+                
+
+            }
+        }
+    }
+}
