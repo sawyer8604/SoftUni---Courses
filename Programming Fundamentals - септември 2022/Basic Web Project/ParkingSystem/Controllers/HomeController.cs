a81bf4b577207000b09627839532e036233ca753
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-12-05 08:43:09 +0200

    initial commit

---
+++ Programming Fundamentals - септември 2022/Basic Web Project/ParkingSystem/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ParkingSystem.Data;
+using ParkingSystem.Data.Models;
+using System.Linq;
+
+namespace ParkingSystem.Controllers
+{
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View(DataAccess.Cars);
+		}
+
+		[HttpPost]
+
+		public IActionResult AddCar(Car car)
+		{
+			DataAccess.Cars.Add(car);
+
+			return Redirect("/");
+		}
+
+        [HttpPost]
+
+        public IActionResult DeleteCar(string plateNumber)
+        {
+			var car = DataAccess.Cars.FirstOrDefault(x => x.PlateNumber == plateNumber);
+			DataAccess.Cars.Remove(car);
+
+			return Redirect("/");
+
+            
+        }
+    }
+
+}
