3fb213dd7ed0b439f0d96e8420c6093419f9bd8f
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-12-05 17:59:36 +0200

    initial commit

---
+++ Programming Fundamentals - септември 2022/02. Programming Fundamentals Mid Exam/03. Numbers 02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> number = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            if(number.Count < 1 )
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            double avarage = number.Average();
+
+
+            List<int> orderedList = number.Where(x => x > avarage).OrderByDescending(y => y).ToList();
+
+            if (orderedList.Count < 1)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            for (int i = 0; i < orderedList.Count; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.Write($"{orderedList[i]} ");
+                
+            }
+
+
+        }
+    }
+}
