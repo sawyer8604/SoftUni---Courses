e4982cb3763167a0f5d913d3cd8a7f015aab0c81
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-12-11 20:48:41 +0200

    Add solutions

---
+++ C# Advanced/02 Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nuberOflines = int.Parse(Console.ReadLine());
+
+            Stack<string> stack = new Stack<string>();
+            
+            string manipulateString = string.Empty;
+
+            for (int i = 0; i < nuberOflines; i++)
+            {
+                string[] commands = Console.ReadLine().Split(" ");
+
+                string commandName = commands[0];
+
+                switch (commandName)
+                {
+                    case "1":
+                        string stringToAppend = commands[1];
+
+                        stack.Push(manipulateString);
+                        manipulateString =  manipulateString.Insert(manipulateString.Length, stringToAppend);
+
+                        break;
+
+                    case "2":
+                        int countToDelete = int.Parse(commands[1]);
+
+                        
+                        if (stack.Any())
+                        {
+                            stack.Push(manipulateString);
+                            int indexToDelete = manipulateString.Length - countToDelete;
+                            manipulateString = manipulateString.Remove(indexToDelete , countToDelete);
+                            
+                        }
+
+                        break;
+
+                    case "3":
+                        int index = int.Parse(commands[1]);
+
+                        char crrString = manipulateString[index - 1];
+                        Console.WriteLine(crrString);
+
+                        break;
+
+                    case "4":
+                        manipulateString = stack.Pop();
+                        
+
+                        break;
+
+
+
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/02 Stacks and Queues - Exercise/10. Crossroads/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._Crossroads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLightTime = int.Parse(Console.ReadLine());
+
+            int freeWindowsTime = int.Parse(Console.ReadLine());
+
+
+            Queue<string> queue = new Queue<string>();
+
+            string commands;
+            int carCounter = 0;
+
+
+
+            while ((commands = Console.ReadLine()) != "END")
+            {
+                if (commands == "green")
+                {
+
+
+                    int crrGreenLight = greenLightTime;
+
+                    while (crrGreenLight > 0 && queue.Any())
+                    {
+                        string crrCar = queue.Dequeue();
+
+                        if (crrGreenLight - crrCar.Length >= 0)
+                        {
+                            carCounter++;
+                            crrGreenLight -= crrCar.Length;
+                            continue;
+
+                        }
+
+                        if (crrGreenLight + freeWindowsTime - crrCar.Length >= 0)
+                        {
+                            crrGreenLight = 0;
+                            carCounter++;
+                            continue;
+
+                        }
+
+                        int hitIndex = crrGreenLight + freeWindowsTime;
+
+                        Console.WriteLine("A crash happened!");
+                        Console.WriteLine($"{crrCar} was hit at {crrCar[hitIndex]}.");
+                        return;
+
+
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(commands);
+                }
+
+
+            }
+
+
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{carCounter} total cars passed the crossroads.");
+
+
+        }
+    }
+}
