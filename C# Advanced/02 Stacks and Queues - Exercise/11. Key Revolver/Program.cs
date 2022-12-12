05e4770f276e782e7edf375572576b89c04bbdf2
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-12-12 03:42:51 +0200

    Add solutions

---
+++ C# Advanced/02 Stacks and Queues - Exercise/11. Key Revolver/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._Key_Revolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int priceOfBullets = int.Parse(Console.ReadLine());
+
+            int gunBarel = int.Parse(Console.ReadLine());
+
+            int[] bullets = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int[] locks = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int intelligence = int.Parse(Console.ReadLine());
+
+
+            Stack<int> stackBullets = new Stack<int>(bullets);
+
+            Queue<int> queueLocks = new Queue<int>(locks);
+
+            int usedBullets = 0;
+
+            while (queueLocks.Any() && stackBullets.Any())
+            {
+
+                usedBullets ++;
+
+                int crrLock = queueLocks.Peek();
+
+                int crrBulet = stackBullets.Pop();
+
+                if (crrBulet <= crrLock)
+                {
+                    queueLocks.Dequeue();
+                    Console.WriteLine("Bang!");
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+
+                if (usedBullets % gunBarel == 0 && stackBullets.Any())
+                {
+                    Console.WriteLine("Reloading!");
+                }
+
+
+            }
+
+
+            if (queueLocks.Any())
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {queueLocks.Count}");
+            }
+            else
+            {
+                Console.WriteLine($"{stackBullets.Count} bullets left. Earned ${intelligence - usedBullets * priceOfBullets}");
+
+            }
+
+
+        }
+    }
+}
---
+++ C# Advanced/02 Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Cups_and_Bottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cupCapacity = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int[] bottle = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Stack<int> bottleStack = new Stack<int>(bottle);
+
+            Queue<int> cupQueue = new Queue<int>(cupCapacity);
+
+            int wastedWater = 0;
+            int needetWater = 0;
+
+            while (bottleStack.Any() && cupQueue.Any())
+            {
+                int crrBottle = bottleStack.Pop();
+
+                int crrCup = cupQueue.Peek();
+
+                needetWater = 0;
+
+                if (crrCup <= crrBottle)
+                {
+                    cupQueue.Dequeue();
+
+                    wastedWater += crrBottle - crrCup;
+
+                    continue;
+                }
+
+                if (crrCup > crrBottle)
+                {
+                    needetWater = crrCup - crrBottle;
+
+                    while (needetWater > 0)
+                    {
+                        needetWater -= crrBottle = bottleStack.Pop();
+
+                    }
+                    cupQueue.Dequeue();
+                    wastedWater += Math.Abs(needetWater);
+
+                }
+
+
+            }
+
+            if (bottleStack.Any())
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottleStack)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+
+            }
+            else
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cupQueue)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+            }
+        }
+    }
+}
