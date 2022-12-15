ef5c962cc2aca54c2c1a8b1c7ed485225485e36d
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-12-15 15:05:21 +0200

    Add solutions

---
+++ C# Advanced/04 Multidimensional Arrays - Exercise/1. Matrix of Palindromes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Matrix_of_Palindromes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int rows = size[0];
+            int cols = size[1];
+
+            string[,] matrix = new string[rows, cols];
+
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] += alphabet[row];
+                    matrix[row, col] += alphabet[col +row];
+                    matrix[row, col] += alphabet[row];
+                }
+
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ C# Advanced/04 Multidimensional Arrays - Exercise/9. Miner/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Linq;
+
+namespace _9._Miner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size, size];
+
+            string[] commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int row = 0; row < size; row++)
+            {
+                char[] crrRow = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(n => char.Parse(n))
+                    .ToArray();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = crrRow[col];
+                }
+            }
+
+            int crRow = 0;
+            int crrCol = 0;
+            int coalCount = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (matrix[row, col] == 's')
+                    {
+                        crRow = row;
+                        crrCol = col;
+                    }
+
+                    if (matrix[row, col] == 'c')
+                    {
+                        coalCount++;
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < commands.Length; i++)
+            {
+                string command = commands[i];
+
+                if (command == "left")
+                {
+                    if (crrCol - 1 >= 0)
+                    {
+                        if (matrix[crRow, crrCol - 1] == '*')
+                        {
+                            matrix[crRow, crrCol - 1] = 's';
+                            matrix[crRow, crrCol] = '*';
+                            crrCol = crrCol - 1;
+                            continue;
+                        }
+
+                        if (matrix[crRow, crrCol - 1] == 'c')
+                        {
+                            coalCount--;
+
+                            matrix[crRow, crrCol - 1] = 's';
+                            matrix[crRow, crrCol] = '*';
+                            crrCol = crrCol - 1;
+
+                            if (coalCount == 0)
+                            {
+                                Console.WriteLine($"You collected all coals! ({crRow}, {crrCol})");
+                                return;
+                            }
+
+                            continue;
+
+                        }
+                        else if (matrix[crRow, crrCol - 1] == 'e')
+                        {
+
+                            crrCol = crrCol - 1;
+                            Console.WriteLine($"Game over! ({crRow}, {crrCol})");
+                            return;
+                        }
+
+                    }
+                }
+                else if (command == "right")
+                {
+                    if (crrCol + 1 < size)
+                    {
+                        if (matrix[crRow, crrCol + 1] == '*')
+                        {
+                            matrix[crRow, crrCol + 1] = 's';
+                            matrix[crRow, crrCol] = '*';
+                            crrCol = crrCol + 1;
+                            continue;
+                        }
+
+                        if (matrix[crRow, crrCol + 1] == 'c')
+                        {
+                            coalCount--;
+
+                            matrix[crRow, crrCol + 1] = 's';
+                            matrix[crRow, crrCol] = '*';
+                            crrCol = crrCol + 1;
+
+                            if (coalCount == 0)
+                            {
+                                Console.WriteLine($"You collected all coals! ({crRow}, {crrCol})");
+                                return;
+                            }
+
+                            continue;
+
+                        }
+
+                        if (matrix[crRow, crrCol + 1] == 'e')
+                        {
+
+                            crrCol = crrCol + 1;
+                            Console.WriteLine($"Game over! ({crRow}, {crrCol})");
+                            return;
+                        }
+
+                    }
+                }
+                else if (command == "up")
+                {
+                    if (crRow - 1 >= 0)
+                    {
+                        if (matrix[crRow - 1, crrCol] == '*')
+                        {
+                            matrix[crRow - 1, crrCol] = 's';
+                            matrix[crRow, crrCol] = '*';
+                            crRow = crRow - 1;
+                            continue;
+                        }
+
+                        if (matrix[crRow - 1, crrCol] == 'c')
+                        {
+                            coalCount--;
+
+                            matrix[crRow - 1, crrCol] = 's';
+                            matrix[crRow, crrCol] = '*';
+                            crRow = crRow - 1;
+
+                            if (coalCount == 0)
+                            {
+                                Console.WriteLine($"You collected all coals! ({crRow}, {crrCol})");
+                                return;
+                            }
+
+                            continue;
+
+                        }
+
+                        if (matrix[crRow - 1, crrCol] == 'e')
+                        {
+
+                            crRow = crRow - 1;
+                            Console.WriteLine($"Game over! ({crRow}, {crrCol})");
+                            return;
+                        }
+
+                    }
+                }
+                else if (command == "down")
+                {
+                    if (crRow + 1 < size)
+                    {
+                        if (matrix[crRow + 1, crrCol] == '*')
+                        {
+                            matrix[crRow + 1, crrCol] = 's';
+                            matrix[crRow, crrCol] = '*';
+                            crRow = crRow + 1;
+                            continue;
+                        }
+
+                        if (matrix[crRow + 1, crrCol] == 'c')
+                        {
+                            coalCount--;
+
+                            matrix[crRow + 1, crrCol] = 's';
+                            matrix[crRow, crrCol] = '*';
+                            crRow = crRow + 1;
+
+                            if (coalCount == 0)
+                            {
+                                Console.WriteLine($"You collected all coals! ({crRow}, {crrCol})");
+                                return;
+                            }
+
+                            continue;
+
+                        }
+
+                        if (matrix[crRow + 1, crrCol] == 'e')
+                        {
+
+                            crRow = crRow + 1;
+                            Console.WriteLine($"Game over! ({crRow}, {crrCol})");
+                            return;
+                        }
+
+                    }
+                }
+
+
+            }
+
+            Console.WriteLine($"{coalCount} coals left. ({crRow}, {crrCol})");
+
+        }
+    }
+}
