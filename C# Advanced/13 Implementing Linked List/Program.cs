30238b6572f99439a78421189c703ca8e00292f9
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-12-26 18:08:05 +0200

    Initial commit

---
+++ C# Advanced/13 Implementing Linked List/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Workshop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddFirst(3);
+            list.AddLast(4);
+            list.AddLast(5);
+            list.AddLast(6);
+
+            list.RemoveFirst();
+            list.RemoveLast();
+
+            list.ForEach(Console.WriteLine);
+        }
+    }
+}
