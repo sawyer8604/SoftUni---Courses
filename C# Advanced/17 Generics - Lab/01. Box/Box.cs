c40444d732ff311f4231e4704b62a5d2e96d5e89
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2022-12-29 20:04:04 +0200

    Add solutions

---
+++ C# Advanced/17 Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> list = new Stack<T>();
+        public int Count { get { return this.list.Count; } }
+        public void Add(T element)
+        {
+            list.Push(element);
+        }
+
+        public T Remove()
+        {
+            return list.Pop();
+        }
+    }
+}
