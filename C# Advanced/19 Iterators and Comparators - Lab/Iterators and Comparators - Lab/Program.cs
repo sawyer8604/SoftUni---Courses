5802dc386f50b771312b5bfa7b4bd6063872db89
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2023-01-03 15:31:53 +0200

    Initial commit

---
+++ C# Advanced/19 Iterators and Comparators - Lab/Iterators and Comparators - Lab/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IntEnumerators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IntEnumerable collections = new IntEnumerable();
+
+            foreach (var item in  collections)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
+
+
+
+class IntEnumerator : IEnumerator<int>
+{
+
+    private int currentNum = 0;
+    public int Current => currentNum;
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()    {  }
+
+    public bool MoveNext()
+    {
+        if(currentNum ++ > 20)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void Reset()
+    {
+        currentNum = 0;
+    }
+}
+
+class IntEnumerable : IEnumerable<int>
+{
+    public IEnumerator<int> GetEnumerator()
+    {
+        return new IntEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ C# Advanced/19 Iterators and Comparators - Lab/MyList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(2);
+            list.Add(2);
+            list.Add(2);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
