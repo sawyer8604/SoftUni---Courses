df17c1434d7f5c734bd4596432f2ccf64c3e1514
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2023-01-08 11:52:09 +0200

    Add solutions

---
+++ C# Advanced/03 Multidimensional Arrays - Lab 01/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int[][] jaggedArray = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] colums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                jaggedArray[row] = colums;
+            }
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] cmdArguments = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string firstCommand = cmdArguments[0];
+
+                int row = int.Parse(cmdArguments[1]);
+                int col = int.Parse(cmdArguments[2]);
+                int value = int.Parse(cmdArguments[3]);
+
+                if (row >= 0 && row < jaggedArray.Length && col >= 0 && col < jaggedArray[row].Length)
+                {
+                    if (firstCommand == "Add")
+                    {
+                        jaggedArray[row][col] += value;
+                    }
+
+                    else if (firstCommand == "Subtract")
+                    {
+                        jaggedArray[row][col] -= value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    Console.Write($"{jaggedArray[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/03 Multidimensional Arrays - Lab 01/7. Pascal Triangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _7._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRows = int.Parse(Console.ReadLine());
+
+            long[][] pascalTriangle = new long[numberOfRows][];
+                       
+
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                pascalTriangle[row] = new long[row + 1];
+
+                long[] crrRow = new long[row + 1];
+
+                crrRow[0] = 1;
+                crrRow[row] = 1;
+                pascalTriangle[row] = crrRow;
+
+                for (int col = 1; col < pascalTriangle[row].Length - 1; col++)
+                {
+                    pascalTriangle[row][col] = pascalTriangle[row - 1][col - 1] + pascalTriangle[row - 1][col];
+                }
+
+                
+            }
+
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                for (int col = 0; col < pascalTriangle[row].Length; col++)
+                {
+                    Console.Write($"{pascalTriangle[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
