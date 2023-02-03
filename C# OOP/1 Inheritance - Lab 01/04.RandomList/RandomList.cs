a595bf271aba8430b9b5812d218a2eabaf9b5848
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2023-02-03 12:53:25 +0200

    Add solutions

---
+++ C# OOP/1 Inheritance - Lab 01/04.RandomList/RandomList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public RandomList()
+        {
+            Random = new Random();
+        }
+
+        public Random Random { get; set; }
+        public string RandomString()
+        {
+            int index = Random.Next(0, this.Count);
+            string str = this[index];
+
+            return str;
+        }
+    }
+}
---
+++ C# OOP/1 Inheritance - Lab 01/05.StackofStrings/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings st= new StackOfStrings();
+
+            List<string> values = new List<string>{"1", "2", "3" };
+
+            Console.WriteLine(st.IsEmpty());
+
+            st.AddRange(values);
+
+            Console.WriteLine(st.IsEmpty());
+
+            Console.WriteLine(string.Join(" ", st));
+        }
+    }
+}
