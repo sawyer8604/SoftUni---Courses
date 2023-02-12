05fbb5d3120ad6e6450115fdb69c64fe1a3402c9
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2023-02-12 10:54:10 +0200

    Add solution

---
+++ C# OOP/4 Encapsulation - Exercise/Football/Player.cs
@@ -0,0 +1,38 @@
+﻿namespace P05.FootballTeamGenerator
+{
+    using System;
+
+    public class Player
+    {
+        private string name;
+
+        public Player(string name, int endurance, int sprint, int dribble, int passing, int shooting)
+        {
+            this.Name = name;
+            this.Stats = new Stats(endurance, sprint, dribble, passing, shooting);
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.NameCannotBeNullOrWhiteSpace);
+                }
+
+                this.name = value;
+            }
+        }
+
+        public Stats Stats { get; private set; }
+
+        public double OverallRating
+            => (this.Stats.Endurance + this.Stats.Sprint + this.Stats.Dribble +
+            this.Stats.Passing + this.Stats.Shooting) / 5.0;
+    }
+}
