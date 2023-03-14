1a85be2a57d044a692365347d1ff5117f1242cc3
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-03-14 13:45:21 +0200

    Add solution

---
+++ C# OOP/12 Reflection and Attribute - Exercise/ValidationAttributes/MyValidationAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public abstract class MyValidationAttribute : Attribute
+    {
+        public abstract bool IsValid(object obj);
+    }
+
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int min;
+        private readonly int max;
+
+        public MyRangeAttribute(int min, int max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+        public override bool IsValid(object obj)
+        {
+            int crrValue = (int)obj;
+
+            return (crrValue >= min && crrValue <= max);
+            
+        }
+    }
+    public class MyRequiredAttribute : MyValidationAttribute
+    {
+        public override bool IsValid(object obj)
+        => obj != null;
+    }
+}
---
+++ C# OOP/12 Reflection and Attribute - Exercise/ValidationAttributes/Validator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class Validator
+    {
+
+        public static bool IsValid(object obj) // Person name, age
+        {
+            PropertyInfo[] propertyInfo = obj.GetType()
+                .GetProperties()
+                .Where(x => x.GetCustomAttributes(typeof(MyValidationAttribute)).Any())
+                .ToArray();
+
+            foreach (var property in propertyInfo)
+            {
+                object value = property.GetValue(obj);
+
+                MyValidationAttribute attribute = property
+                    .GetCustomAttribute<MyValidationAttribute>();
+
+                bool IsValid = attribute.IsValid(value);
+
+                if(!IsValid)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
