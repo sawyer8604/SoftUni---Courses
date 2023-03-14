7914832a0d0c3cdebddcf0eeb56292fe1a5a6264
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-03-14 11:55:10 +0200

    Add solution

---
+++ C# OOP/12 Reflection and Attribute - Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,42 @@
+﻿using CommandPattern.Core.Commands;
+using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] input = args.Split();
+
+            string commandName = input[0];
+            string[] value = input.Skip(1).ToArray();
+
+            Type type = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(x => x.Name == commandName + "Command");
+
+            if(type == null)
+            {
+                throw new InvalidOperationException("Missing command");
+            }
+
+            Type commandInterfaces = type.GetInterface("ICommand");
+
+            if (commandInterfaces == null)
+            {
+                throw new InvalidOperationException("Not a command");
+            }
+
+            var command = Activator.CreateInstance(type) as ICommand;
+
+            string result = command.Execute(value);
+
+            return result;
+        }
+    }
+}
