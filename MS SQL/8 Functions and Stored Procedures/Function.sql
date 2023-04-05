069b871cf757ba7a2558055b56a202c241602bc1
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-04-05 17:05:42 +0300

    Initial commit

---
+++ MS SQL/8 Functions and Stored Procedures/Function.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION udf_EmployeesByYear(@Year SMALLINT)
+RETURNS TABLE
+AS
+RETURN
+(
+SELECT * FROM  Employees
+WHERE YEAR(HireDate) = @Year
+)
+
+
+SELECT * FROM udf_EmployeesByYear(2001)
