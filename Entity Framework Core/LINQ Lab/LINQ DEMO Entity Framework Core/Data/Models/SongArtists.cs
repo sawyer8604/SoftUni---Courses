0bb4ba43fd8ed7817741e1d0b0781bfcc3e0f2dd
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-04-22 17:59:13 +0300

    Initial commit

---
+++ Entity Framework Core/LINQ Lab/LINQ DEMO Entity Framework Core/Data/Models/SongArtists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LINQ_DEMO_Entity_Framework_Core.Data.Models
+{
+    public partial class SongArtists
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedOn { get; set; }
+        public int SongId { get; set; }
+        public int ArtistId { get; set; }
+        public int Order { get; set; }
+
+        [ForeignKey(nameof(ArtistId))]
+        [InverseProperty(nameof(Artists.SongArtists))]
+        public virtual Artists Artist { get; set; }
+        [ForeignKey(nameof(SongId))]
+        [InverseProperty(nameof(Songs.SongArtists))]
+        public virtual Songs Song { get; set; }
+    }
+}
---
+++ Entity Framework Core/LINQ Lab/LINQ DEMO Entity Framework Core/Data/Models/SongMetadata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LINQ_DEMO_Entity_Framework_Core.Data.Models
+{
+    public partial class SongMetadata
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedOn { get; set; }
+        public int SongId { get; set; }
+        public short Type { get; set; }
+        public string Value { get; set; }
+        public int? SourceId { get; set; }
+        public string SourceItemId { get; set; }
+
+        [ForeignKey(nameof(SongId))]
+        [InverseProperty(nameof(Songs.SongMetadata))]
+        public virtual Songs Song { get; set; }
+        [ForeignKey(nameof(SourceId))]
+        [InverseProperty(nameof(Sources.SongMetadata))]
+        public virtual Sources Source { get; set; }
+    }
+}
