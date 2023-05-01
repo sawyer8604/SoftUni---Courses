fc6a600b87c9a55692e206a0fdb74c907a34e7e2
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-05-01 13:34:39 +0300

    Initial commit - Export Data

---
+++ Entity Framework Core/12 JSON Exercises/Car Dealer Export Data/ProductShop/DataTransferObjects/CategoryAndProductInputModel.cs
@@ -0,0 +1,15 @@
+﻿using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductShop.DataTransferObjects
+{
+    public class CategoryAndProductInputModel
+    {
+        public int CategoryId { get; set; }
+
+        public int ProductId { get; set; }
+
+    }
+}
---
+++ Entity Framework Core/12 JSON Exercises/Car Dealer Export Data/ProductShop/DataTransferObjects/CategoryInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductShop.DataTransferObjects
+{
+    public class CategoryInputModel
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
