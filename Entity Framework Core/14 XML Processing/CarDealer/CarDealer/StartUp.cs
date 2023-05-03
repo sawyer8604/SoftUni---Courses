03e67e734940dafef01ab1e7cd64cb951c453833
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-05-03 12:28:04 +0300

    Add solutions

---
+++ Entity Framework Core/14 XML Processing/CarDealer/CarDealer/StartUp.cs
@@ -0,0 +1,379 @@
+﻿namespace CarDealer;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using DTOs.Export;
+using Data;
+using Models;
+
+
+public class StartUp
+{
+    
+    public static void Main()
+    {
+        CarDealerContext context = new CarDealerContext();
+
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        // I. Import Data
+
+        // problem 09
+        string xmlSuppliers = File.ReadAllText(@"../../../Datasets/suppliers.xml");
+        var suppliersResult  = ImportSuppliers(context, xmlSuppliers);
+
+        //problem 10
+        string xmlParts = File.ReadAllText(@"../../../Datasets/parts.xml");
+        var partsResult =  ImportParts(context, xmlParts);
+
+        //problem 11.
+        string xmlCars = File.ReadAllText(@"../../../Datasets/cars.xml");
+        var carsResult = ImportCars(context, xmlCars);
+
+        //problem 12.
+        string xmlCustomers = File.ReadAllText(@"../../../Datasets/customers.xml");
+        var customersResult = ImportCustomers(context, xmlCustomers);
+
+        //problem 13.
+        string xmlSales = File.ReadAllText(@"../../../Datasets/sales.xml");
+        var salesResult = ImportSales(context, xmlSales);
+
+        Console.WriteLine(carsResult);
+
+        // II. Export Data
+
+        //problem 14.
+        //Console.WriteLine(GetCarsWithDistance(context));
+        //File.WriteAllText(@"../../../Results/cars.xml", GetCarsWithDistance(context));
+
+        //problem 15.
+        //Console.WriteLine(GetCarsFromMakeBmw(context));
+        //File.WriteAllText(@"../../../Results/bmw-cars.xml", GetCarsFromMakeBmw(context));
+
+        //problem 16.
+        //Console.WriteLine(GetLocalSuppliers(context));
+        //File.WriteAllText(@"../../../Results/local-suppliers.xml", GetLocalSuppliers(context));
+
+        //problem 17.
+        //Console.WriteLine(GetCarsWithTheirListOfParts(context));
+        //File.WriteAllText(@"../../../Results/cars-and-parts.xml", GetCarsWithTheirListOfParts(context));
+
+        //problem 18.
+        //Console.WriteLine(GetTotalSalesByCustomer(context));
+        //File.WriteAllText(@"../../../Results/customers-total-sales.xml", GetTotalSalesByCustomer(context));
+
+        //problem 19.
+        // Console.WriteLine(GetSalesWithAppliedDiscount(context));
+        //File.WriteAllText(@"../../../Results/sales-discounts.xml", GetSalesWithAppliedDiscount(context));
+    }
+
+    private static string Serializer<T>(T dataTransferObjects, string xmlRootAttributeName)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+        StringBuilder sb = new StringBuilder();
+        using var write = new StringWriter(sb);
+
+        XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+        xmlNamespaces.Add(string.Empty, string.Empty);
+
+        serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+        return sb.ToString();
+    }
+
+    // 09. Import Suppliers
+    public static string ImportSuppliers(CarDealerContext context, string inputXml)
+    {
+        XDocument xmDocument = XDocument.Parse(inputXml);
+
+        var suppliers = xmDocument.Root.Elements();
+
+        foreach (var supplier in suppliers)
+        {
+            Supplier s = new Supplier()
+            {
+                Name = supplier.Element("name").Value,
+                IsImporter = bool.Parse(supplier.Element("isImporter").Value)
+            };
+
+            context.Suppliers.Add(s);
+        }
+
+        context.SaveChanges();
+
+        return $"Successfully imported {suppliers.Count()}";
+    }
+
+    // 10. Import Parts
+    
+    public static string ImportParts(CarDealerContext context, string inputXml)
+    {
+        XDocument xmDocument = XDocument.Parse(inputXml);
+
+        var parts = xmDocument.Root.Elements();
+
+        foreach (var part in parts)
+        {
+            int suppId = int.Parse(part.Element("supplierId").Value);
+            if (!context.Suppliers.Any(s => s.Id == suppId))
+            {
+                continue;
+            }
+
+            Part p = new Part()
+            {
+                Name = part.Element("name").Value,
+                Price = decimal.Parse(part.Element("price").Value),
+                Quantity = int.Parse(part.Element("quantity").Value),
+                SupplierId = int.Parse(part.Element("supplierId").Value)
+            };
+
+            context.Parts.Add(p);
+        }
+
+        context.SaveChanges();
+
+        return $"Successfully imported {context.Parts.Count()}";
+    }
+
+    // 11. Import Cars
+    public static string ImportCars(CarDealerContext context, string inputXml)
+    {
+        XDocument xmlDocument = XDocument.Parse(inputXml);
+
+        var cars = xmlDocument.Root.Elements();
+
+        List<Car> carsX = new List<Car>();
+        List<PartCar> partsX = new List<PartCar>();
+
+        int carId = 1;
+
+        int[] partInts = context.Parts.Select(p => p.Id).ToArray();
+        var partIds = xmlDocument.Descendants("partId").Select(p => p.Attribute("id").Value);
+
+        foreach (var car in cars)
+        {
+            Car c = new Car()
+            {
+                Make = car.Element("make").Value,
+                Model = car.Element("model").Value,
+                TraveledDistance = long.Parse(car.Element("traveledDistance").Value)
+                                
+            };
+
+            carsX.Add(c);
+
+            foreach (var partId in car.Descendants("partId")
+                         .Where(p => partIds.Contains(p.Attribute("id").Value))
+                         .Select(p => p.Attribute("id").Value).Distinct())
+            {
+                PartCar partCar = new PartCar()
+                {
+                    CarId = carId,
+                    Car = c,
+                    PartId = int.Parse(partId)
+                };
+
+                partsX.Add(partCar);
+            }
+
+            carId++;
+        }
+
+        context.AddRange(carsX);
+        context.AddRange(partsX);
+
+        context.SaveChanges();
+
+        return $"Successfully imported {carsX.Count}";
+    }
+
+    // 12. Import Customers
+    public static string ImportCustomers(CarDealerContext context, string inputXml)
+    {
+        XDocument xmlDocument = XDocument.Parse(inputXml);
+
+        var customers = xmlDocument.Root.Elements();
+
+        foreach (var customer in customers)
+        {
+            Customer c = new Customer()
+            {
+                Name = customer.Element("name").Value,
+                BirthDate = DateTime.Parse(customer.Element("birthDate").Value),
+                IsYoungDriver = bool.Parse(customer.Element("isYoungDriver").Value)
+            };
+
+            context.Customers.Add(c);
+        }
+
+        context.SaveChanges();
+
+        return $"Successfully imported {customers.Count()}";
+    }
+
+    // 13. Import Sales
+    public static string ImportSales(CarDealerContext context, string inputXml)
+    {
+        XDocument xmlDocument = XDocument.Parse(inputXml);
+
+        var sales = xmlDocument.Root.Elements();
+
+        int[] allCarIds = context.Cars.Select(c => c.Id).ToArray();
+
+        foreach (var sale in sales)
+        {
+            if (!allCarIds.Contains(int.Parse(sale.Element("carId").Value)))
+            {
+                continue;
+            }
+
+            Sale s = new Sale()
+            {
+                CarId = int.Parse(sale.Element("carId").Value),
+                CustomerId = int.Parse(sale.Element("customerId").Value),
+                Discount = decimal.Parse(sale.Element("discount").Value)
+            };
+
+            context.Sales.Add(s);
+        }
+
+        context.SaveChanges();
+
+        return $"Successfully imported {context.Sales.Count()}";
+    }
+
+    // 14. Export Cars With Distance
+    public static string GetCarsWithDistance(CarDealerContext context)
+    {
+        var cars = context.Cars
+            .Where(c => c.TraveledDistance > 2000000)
+            .OrderBy(c => c.Make)
+            .ThenBy(c => c.Model)
+            .Take(10)
+            .Select(c => new ExportCarsWithDistanceDto()
+            {
+                Make = c.Make,
+                Model = c.Model,
+                TraveledDistance = c.TraveledDistance,
+            })
+            .ToArray();
+
+        return Serializer<ExportCarsWithDistanceDto[]>(cars, "cars");
+    }
+
+    // 15. Export Cars from Make BMW
+    public static string GetCarsFromMakeBmw(CarDealerContext context)
+    {
+        var bmwCars = context.Cars
+            .Where(c => c.Make == "BMW")
+            .OrderBy(c => c.Model)
+            .ThenByDescending(c => c.TraveledDistance)
+            .Select(c => new ExportCarsFromMakeBMWDto()
+            {
+                Id = c.Id,
+                Model = c.Model,
+                TraveledDistance = c.TraveledDistance
+            })
+            .ToArray();
+
+        return Serializer<ExportCarsFromMakeBMWDto[]>(bmwCars, "cars");
+    }
+
+    // 16. Export Local Suppliers
+    public static string GetLocalSuppliers(CarDealerContext context)
+    {
+        var localSuppliers = context.Suppliers
+            .Where(s => !s.IsImporter)
+            .Select(s => new ExportLocalSuppliersDto()
+            {
+                Id = s.Id,
+                Name = s.Name,
+                PartsCount = s.Parts.Count
+            })
+            .ToArray();
+
+        return Serializer<ExportLocalSuppliersDto[]>(localSuppliers, "suppliers");
+    }
+
+    // 17. Export Cars with Their List of Parts
+    public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+    {
+        var carsWithParts = context.Cars
+            .OrderByDescending(c => c.TraveledDistance)
+            .ThenBy(c => c.Model)
+            .Take(5)
+            .Select(c => new ExportCarsWithTheirListOfPartsDto()
+            {
+                Make = c.Make,
+                Model = c.Model,
+                TraveledDistance = c.TraveledDistance,
+                PartsFromCars = c.PartsCars
+                    .Select(p => new PartsFromCars()
+                    {
+                        Name = p.Part.Name,
+                        Price = p.Part.Price,
+                    })
+                    .OrderByDescending(p => p.Price)
+                    .ToArray()
+            })
+            .ToArray();
+
+        return Serializer<ExportCarsWithTheirListOfPartsDto[]>(carsWithParts, "cars");
+    }
+
+    // 18. Export Total Sales by Customer
+    public static string GetTotalSalesByCustomer(CarDealerContext context)
+    {
+        var customerSales = context.Customers
+            .Where(cus => cus.Sales.Any())
+            .Select(cus => new
+            {
+                fullName = cus.Name,
+                boughtCars = cus.Sales.Count(),
+                moneyCars = cus.IsYoungDriver
+                    ? cus.Sales.SelectMany(s => s.Car.PartsCars.Select(p => Math.Round(p.Part.Price * 0.95m, 2)))
+                    : cus.Sales.SelectMany(s => s.Car.PartsCars.Select(p => Math.Round(p.Part.Price, 2)))
+            })
+            .ToArray();
+
+        var output = customerSales
+            .Select(o => new ExportTotalSalesByCustomerDto()
+            {
+                FullName = o.fullName,
+                BoughtCars = o.boughtCars,
+                SpentMoney = o.moneyCars.Sum()
+            })
+            .OrderByDescending(o => o.SpentMoney)
+            .ToArray();
+
+        return Serializer<ExportTotalSalesByCustomerDto[]>(output, "customers");
+    }
+
+    // 19. Export Sales with Applied Discount
+    public static string GetSalesWithAppliedDiscount(CarDealerContext context)
+    {
+        var sales = context.Sales
+            .Select(s => new ExportSalesWithAppliedDiscountDto()
+            {
+                CarDtoSales = new CarDtoSales()
+                {
+                    Make = s.Car.Make,
+                    Model = s.Car.Model,
+                    TraveledDistance = s.Car.TraveledDistance
+                },
+
+                Discount = (int)s.Discount,
+                CustomerName = s.Customer.Name,
+                Price = s.Car.PartsCars.Sum(p => p.Part.Price).ToString("0.00"),
+                PriceWithDiscount = Math.Round((double)(s.Car.PartsCars.Sum(p => p.Part.Price) * (1 - (s.Discount / 100))), 4)
+            })
+            .ToArray();
+
+        return Serializer<ExportSalesWithAppliedDiscountDto[]>(sales, "sales");
+    }
+}
---
+++ Entity Framework Core/14 XML Processing/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,269 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ProductShop;
+
+using System.Text;
+using System.Xml.Linq;
+using DTOs.Export;
+using Data;
+using Models;
+using System.Xml.Serialization;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        ProductShopContext context = new ProductShopContext();
+
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        // I. Import Data
+
+        //problem 01.
+        string xmlUsers = @"../../../Datasets/users.xml";
+        ImportUsers(context, xmlUsers);
+
+        //problem 02.
+        string xmlProducts = File.ReadAllText(@"../../../Datasets/products.xml");
+        ImportProducts(context, xmlProducts);
+
+        //problem 03.
+        string xmlCategories = File.ReadAllText(@"../../../Datasets/categories.xml");
+        ImportCategories(context, xmlCategories);
+
+        //problem 04.
+        string xmlCategoryProducts = File.ReadAllText(@"../../../Datasets/categories-products.xml");
+        ImportCategoryProducts(context, xmlCategoryProducts);
+
+        // II. Export Data
+
+        //problem 05.
+        string xmlProductsInRange = GetProductsInRange(context);
+        File.WriteAllText(@"../../../Results/products-in-range.xml", xmlProductsInRange);
+
+        //problem 06.
+        string xmlSoldProducts = GetSoldProducts(context);
+        File.WriteAllText(@"../../../Results/users-sold-products.xml", xmlSoldProducts);
+
+        //problem 07.
+        string xmlCategoriesByProductsCount = GetCategoriesByProductsCount(context);
+        File.WriteAllText(@"../../../Results/categories-by-products.xml", xmlCategoriesByProductsCount);
+
+        //problem 08.
+        string xmlUsersWithProducts = GetUsersWithProducts(context);
+        File.WriteAllText(@"../../../Results/users-and-products.xml", xmlUsersWithProducts);
+
+    }
+
+    private static string Serializer<T>(T dataTransferObjects, string xmlRootAttributeName)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+        StringBuilder sb = new StringBuilder();
+        using var write = new StringWriter(sb);
+
+        XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+        xmlNamespaces.Add(string.Empty, string.Empty);
+
+        serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+        return sb.ToString();
+    }
+
+    // 01. Import Users
+    public static string ImportUsers(ProductShopContext context, string inputXml)
+    {
+        XDocument xmlDocument = XDocument.Load(inputXml);
+
+        var users = xmlDocument.Root.Elements();
+
+        foreach (var user in users)
+        {
+            User u = new User()
+            {
+                FirstName = user.Element("firstName").Value,
+                LastName = user.Element("lastName").Value,
+                Age = int.Parse(user.Element("age").Value)
+            };
+
+            context.Users.Add(u);
+        }
+
+        context.SaveChanges();
+
+        return $"Successfully imported {users.Count()}";
+    }
+
+    // 02. Import Products
+    public static string ImportProducts(ProductShopContext context, string inputXml)
+    {
+        XDocument xmlDocument = XDocument.Parse(inputXml);
+
+        var products = xmlDocument.Root.Elements();
+
+        foreach (var product in products)
+        {
+            Product p = new Product()
+            {
+                Name = product.Element("name").Value,
+                Price = decimal.Parse(product.Element("price").Value),
+                SellerId = int.Parse(product.Element("sellerId").Value),
+                BuyerId = product.Elements().Count() > 3 ? int.Parse(product.Element("buyerId")!.Value) : null
+            };
+
+            context.Products.Add(p);
+        }
+
+        context.SaveChanges();
+
+        return $"Successfully imported {products.Count()}";
+    }
+
+    // 03. Import Categories
+    public static string ImportCategories(ProductShopContext context, string inputXml)
+    {
+        XDocument xmlDocument = XDocument.Parse(inputXml);
+
+        var categories = xmlDocument.Root.Elements();
+
+        foreach (var category in categories)
+        {
+            Category c = new Category()
+            {
+                Name = category.Element("name").Value
+            };
+
+            context.Categories.Add(c);
+        }
+
+        context.SaveChanges();
+
+        return $"Successfully imported {categories.Count()}";
+    }
+
+    // 04. Import Categories and Products
+    public static string ImportCategoryProducts(ProductShopContext context, string inputXml)
+
+
+    {
+        XDocument xmDocument = XDocument.Parse(inputXml);
+
+        var categoryProducts = xmDocument.Root.Elements();
+
+        foreach (var categoryProduct in categoryProducts)
+        {
+            CategoryProduct cp = new CategoryProduct()
+            {
+                CategoryId = int.Parse(categoryProduct.Element("CategoryId").Value),
+                ProductId = int.Parse(categoryProduct.Element("ProductId").Value)
+            };
+
+            context.CategoryProducts.Add(cp);
+        }
+
+        context.SaveChanges();
+
+        return $"Successfully imported {categoryProducts.Count()}";
+    }
+
+    // 05. Export Products In Range
+    public static string GetProductsInRange(ProductShopContext context)
+    {
+        var productsInRange = context.Products
+            .Where(p => p.Price >= 500 && p.Price <= 1000)
+            .OrderBy(p => p.Price)
+            .Take(10)
+            .Select(p => new ExportProductsInRangeDto()
+            {
+                Price = p.Price,
+                Name = p.Name,
+                Buyer = p.Buyer.FirstName + " " + p.Buyer.LastName
+            })
+            .ToArray();
+
+        return Serializer<ExportProductsInRangeDto[]>(productsInRange, "Products");
+    }
+
+    // 06. Export Sold Products
+    public static string GetSoldProducts(ProductShopContext context)
+    {
+        var products = context.Users
+            .Where(u => u.ProductsSold.Any(p => p.BuyerId != null))
+            .OrderBy(u => u.LastName)
+            .ThenBy(u => u.FirstName)
+            .Take(5)
+            .Select(u => new ExportSoldProductsDto()
+            {
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Products = u.ProductsSold
+                    .Select(p => new ProductDto()
+                    {
+                        Name = p.Name,
+                        Price = p.Price
+                    })
+                    .ToArray()
+            })
+            .ToArray();
+
+        return Serializer<ExportSoldProductsDto[]>(products, "Users");
+    }
+
+    // 07. Export Categories By Products Count
+    public static string GetCategoriesByProductsCount(ProductShopContext context)
+    {
+        var categories = context.Categories
+            .Select(c => new ExportCategoriesByProductsCountDto()
+            {
+                Name = c.Name,
+                Count = c.CategoryProducts.Count,
+                AveragePrice = c.CategoryProducts.Average(p => p.Product.Price),
+                TotalRevenue = c.CategoryProducts.Sum(p => p.Product.Price)
+            })
+            .OrderByDescending(p => p.Count)
+            .ThenBy(p => p.TotalRevenue)
+            .ToArray();
+
+        return Serializer<ExportCategoriesByProductsCountDto[]>(categories, "Categories");
+    }
+
+    // 08. Export Users and Products
+    [SuppressMessage("ReSharper.DPA", "DPA0000: DPA issues")]
+    public static string GetUsersWithProducts(ProductShopContext context)
+    {
+        var users = context.Users
+            .Where(u => u.ProductsSold.Any(p => p.BuyerId != null))
+            .OrderByDescending(u => u.ProductsSold.Count(p => p.BuyerId != null))
+            .Select(u => new UserInfo()
+            {
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Age = u.Age,
+                SoldProducts = new SoldProducts()
+                {
+                    Count = u.ProductsSold.Count(p => p.BuyerId != null),
+                    Products = u.ProductsSold
+                        .Where(p => p.BuyerId != null)
+                        .Select(p => new ProductX()
+                        {
+                            Name = p.Name,
+                            Price = p.Price
+                        })
+                        .OrderByDescending(p => p.Price)
+                        .ToArray()
+                }
+            })
+            .Take(10)
+            .ToArray();
+
+        ExportUsersAndProductsDto info = new ExportUsersAndProductsDto()
+        {
+            Count = context.Users.Count(u => u.ProductsSold.Any(p => p.BuyerId != null)),
+            Users = users
+        };
+
+        return Serializer<ExportUsersAndProductsDto>(info, "Users");
+    }
+
+}
