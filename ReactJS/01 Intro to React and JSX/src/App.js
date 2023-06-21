16ed93adf7c2c15ca0922cb82c30a88adb869638
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-06-21 15:26:09 +0300

    initial commit

---
+++ ReactJS/01 Intro to React and JSX/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import EventInfo from './components/EventInfo';
+import Speakers from './components/Speakers';
+import Tickets from './components/Tickets';
+import Schedule from './components/Schedule';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div >              
+      <Navigation/>
+
+      <Header/>
+
+      <div className="container">
+        
+       <EventInfo/>
+        
+        <Speakers/>
+        
+      </div>
+      
+      <Tickets/>
+    
+      <Schedule/>
+
+      <Footer/>
+        
+      
+    </div>
+  );
+}
+
+export default App;
