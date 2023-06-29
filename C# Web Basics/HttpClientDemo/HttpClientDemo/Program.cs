8bddf49610a7a87d9e16500aa0d2a118ff6ac6ec
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-06-29 13:10:09 +0300

    initial commit

---
+++ C# Web Basics/HttpClientDemo/HttpClientDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string url = "https://softuni.bg/";
+            HttpClient httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            Console.Write(html);
+        }
+    }
+}
