394ba8fbc41e8aeb84a6c7533dc08447ec016705
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-09-03 17:03:31 +0300

    Demo View and View Model

---
+++ ASP.NET Core/MyFirstWebApp/MyFirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApp.Data;
+using MyFirstWebApp.Models;
+using MyFirstWebApp.ViewModels.Home;
+using System.Diagnostics;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext dbContext;
+
+        public HomeController(ILogger<HomeController> logger,
+            ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var viewModel = new IndexViewModel
+            {
+                Id = id,
+                Name = "Mus",
+                Year = DateTime.UtcNow.Year,
+                ProcessorCount = Environment.ProcessorCount,
+                UsersCount = dbContext.Users.Count()
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET Core/MyFirstWebApp/MyFirstWebApp/ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MyFirstWebApp.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        public int Year { get; set; }
+        public int UsersCount { get; set; }
+        public int ProcessorCount { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+
+    }
+}
